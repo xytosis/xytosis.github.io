814ea5913555516b7f9a3500e063aecedfa06de1
Martin Zhu <martin_zhu@brown.edu>
2015-06-24 18:43:57 -0400

    made mobile one almost

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /* This script will contain formatting for main page*/
 
 $(document).ready(function() {
+	document.body.style.overflow = "hidden";
+	document.body.style.overflow = "visible";
 	$("#mobilemenu").click(function() {
 		var headerbar = $("#mobilemenubar");
 		if (headerbar.css("visibility") == "visible") {
